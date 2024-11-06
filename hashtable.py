26cace7a3b45378ce438725772686b49824a0103
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-11-06 15:57:58 +0000

    Linear Probing

--- hashtable.py
+++ hashtable.py
@@ -2,40 +2,35 @@ from linkedlist import TuplesLinkedList
 
 class HashTable:
     def __init__(self,):
-        self.buckets = [TuplesLinkedList() for i in range(127)]
+        self.internal_list = [None for i in range(127)]
 
     def hash(self, key):
-        totalhash = ""
+        totalhash = 0
         for i in range(len(key)):
             for char in key:
-                totalhash += (ord(char) * 31) ^ i
+                totalhash += ord(char) * (31 ** i)
             return totalhash
 
-    # def unhash(self, hash):
-    #     totalunhash = ""
-    #     for i in range(len(hash)):
-    #         for char in hash:
-    #             totalunhash += (ord(char) * 31) ^ i
-
-
-
     def put(self, key, value):
-        self.buckets[hash(key) % 127].add(key, value)
+        bucket_index = hash(key) % 127
+        index = bucket_index
+        stop = False
+        while not stop:
+            if self.internal_list[index] is None:
+                self.internal_list[index] = (key, value)
+                stop = True
+            else:
+                index += bucket_index % 17
+
+
+
 
     def get(self, key):
-        for i in range(self.buckets[hash(key) % 127].size):
-            total = ""
-            for g in range(len(key)):
-                total += self.buckets[hash(key) % 127].get(i)[g]
-        return total
+        counter = hash(key) % 127
+        while self.internal_list[counter][0] != key:
+            hash2 = counter % 17
+        return self.internal_list[counter][1]
 
 
-    # def get(self, key):
-    #     for i in range(self.buckets[hash(key) % 127].size):
-    #         total = ""
-    #         total += self.buckets[hash(key) % 127].get(i)
-    #     return total
-    #
-    # def get(self, key):
-    #     self.buckets[hash(key) % 127].find(key)
+
 
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ HashTableTest.put("Cat","It meows")
 
 HashTableTest.put("Cta","Not a real word")
 
+HashTableTest.put("aCt","Not a real word2")
+
 HashTableTest.put("hi","Hello!")
 
-print(HashTableTest.get("Cta"))
+print(HashTableTest.get("aCt"))
